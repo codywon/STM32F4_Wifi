2dd694e72a4e1f3e212c99fc50f0ebb37684602a
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-02-18 16:51:57 +0100

    modificato header nRF24L01P.h, sistemati metodi pubblici e privati
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- nRF24L01P.h
+++ nRF24L01P.h
@@ -19,40 +19,39 @@ public:
     void power_down();
     void set_transmit_mode();
     void set_receive_mode();
-    int transmit(int count, char* data);
-    int receive(char *data,int count);
+    void set_crc_width(int width);
+    void set_tx_address(unsigned long long address, int width);
+    void set_rx_address_pipe0(unsigned long long address, int width);
+    void set_frequency(int frequency);
+    void set_power_output(int power);
+    void set_air_data_rate(int rate);
+    void set_tx_address(int number);
+    void set_transfer_size(int size);
     int get_frequency();
     int get_air_data_rate(); 
     int get_output_power();
     int get_crc_width();
-    void disable_auto_ack();
-    void disable_auto_retransmit();
     unsigned long long get_tx_address();
     unsigned long long get_rx_address_pipe0();
     int get_register_status();
     void reset_interrupt();
     bool packet_in_pipe0();
-
-
+    void disable_auto_ack();
+    void disable_auto_retransmit();
+    void disable_tx_interrupt();
+    int transmit(int count, char* data);
+    int receive(char *data,int count);
+    
 
 private:
     void clear_pending_interrupt();
-    void disable_tx_interrupt();
     void CE_enable();
     void CE_disable();
     void set_register(int addr_registro,int data_registro);
     int get_register(int registro);
     void CE_restore(int old_ce);
     void setup_Gpio();
-    void set_frequency(int frequency);
-    void set_power_output(int power);
-    void set_air_data_rate(int rate);
-    void set_tx_address(int number);
     void flush_tx();
-    void set_crc_width(int width);
-    void set_tx_address(unsigned long long address, int width);
-    void set_rx_address_pipe0(unsigned long long address, int width);
-    void set_transfer_size(int size);
     int mode;
     spi_driver *spi;
 
